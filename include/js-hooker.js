0b77822bb507c9baf59526afb3d0a0c97801f354
DosX <admin@kay-software.ru>
2024-12-17 11:55:39 +0300

    Update js-hooker.js

--- include/js-hooker.js
+++ include/js-hooker.js
@@ -21,48 +21,52 @@ function watchAllFunctions(obj, exclude = []) {
         if (typeof obj[key] === 'function' && !exclude.includes(key)) {
             const originalFunction = obj[key];
 
-            if (obj[key] === watchAllFunctions) {
+            if (obj[key] === watchAllFunctions || obj[key] === getHookedFunction) {
                 continue;
             }
 
-            obj[key] = function() {
-                "[native code]";
-                const args = Array.from(arguments).map(arg => {
-                    if (typeof arg === 'string') {
-                        return `"${arg}"`;
-                    } else if (typeof arg === 'object') {
-                        try {
-                            return JSON.stringify(arg);
-                        } catch (e) {
-                            return '[Circular]';
-                        }
-                    } else {
-                        return arg;
-                    }
-                });
-
-                if (!(key in nativeFunctionCache)) {
-                    nativeFunctionCache[key] = !!originalFunction && (typeof originalFunction).toLowerCase() === 'function' &&
-                        (originalFunction === Function.prototype ||
-                            /^\s*function\s*(\b[a-z$_][a-z0-9$_]*\b)*\s*\((|([a-z$_][a-z0-9$_]*)(\s*,[a-z$_][a-z0-9$_]*)*)\)\s*{\s*\[native code\]\s*}\s*$/i.test(String(originalFunction)));
-                }
-
-                const isNativeFunction = nativeFunctionCache[key];
-
-                console.log(
-                    `%c${isNativeFunction ? '[N]' : '[C]'}%c %c[CALL CAPTURED]%c : ${key}(` + args.join(', ') + ");",
-                    `color: white; background: ${isNativeFunction ? 'red' : 'gray'};`,
-                    'color: default;',
-                    'color: white; background: darkorange;',
-                    'color: default;'
-                );
-
-                if (originalFunction) {
-                    return Function.prototype.apply.call(originalFunction, this, arguments);
-                }
-            };
+            obj[key] = getHookedFunction(originalFunction);
         }
     }
+}
+
+function getHookedFunction(originalFunction) {
+    return function() {
+        "[native code]";
+        const args = Array.from(arguments).map(arg => {
+            if (typeof arg === 'string') {
+                return `"${arg}"`;
+            } else if (typeof arg === 'object') {
+                try {
+                    return JSON.stringify(arg);
+                } catch (e) {
+                    return '[Circular]';
+                }
+            } else {
+                return arg;
+            }
+        });
+
+        if (!(originalFunction.name in nativeFunctionCache)) {
+            nativeFunctionCache[originalFunction.name] = !!originalFunction && (typeof originalFunction).toLowerCase() === 'function' &&
+                (originalFunction === Function.prototype ||
+                    /^\s*function\s*(\b[a-z$_][a-z0-9$_]*\b)*\s*\((|([a-z$_][a-z0-9$_]*)(\s*,[a-z$_][a-z0-9$_]*)*)\)\s*{\s*\[native code\]\s*}\s*$/i.test(String(originalFunction)));
+        }
+
+        const isNativeFunction = nativeFunctionCache[originalFunction.name];
+
+        console.log(
+            `%c${isNativeFunction ? '[N]' : '[C]'}%c %c[CALL CAPTURED]%c : ${originalFunction.name}(` + args.join(', ') + ");",
+            `color: white; background: ${isNativeFunction ? 'red' : 'gray'};`,
+            'color: default;',
+            'color: white; background: darkorange;',
+            'color: default;'
+        );
+
+        if (originalFunction) {
+            return Function.prototype.apply.call(originalFunction, this, arguments);
+        }
+    };
 }
 
 // Change depending on what you need to track.
